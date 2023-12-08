c61bff6add14420f0934f151e65817d44e897640
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-12-09 00:46:50 +0530

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './Navbar/Navbar';
 
 function App() {
+  const [tickets, setTickets] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, [])
+  const fetchData = async () => {
+    try {
+      const res = await fetch('https://api.quicksell.co/v1/internal/frontend-assignment');
+      const data = await res.json();
+      setTickets(data);
+    } catch (error) {
+      console.log("Unable to fetch data! ", error);
+    }
+  }
+
   return (
     <div className="App">
       <Navbar />
